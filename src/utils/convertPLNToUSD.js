12f3c1865ecb05fdf25b7d828ed1e21f43164f77
Radoslaw Wieczorek <r.p.wieczorek@gmail.com>
2022-02-10 22:23:47 +0100

    Exercise 15.1

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,4 +1,24 @@
 export const convertPLNToUSD = (PLN) => {
+  if (typeof(PLN) == "string") {
+    return NaN;
+  }
+  
+  if (PLN === undefined) {
+    return NaN;
+  }
+
+  if (typeof(PLN) === "object") {
+    return "Error";
+  }
+  
+  if (typeof(PLN) === "function") {
+    return "Error";
+  }
+
+  if (PLN < 0) {
+    return convertPLNToUSD(0);
+  }
+  
   const PLNtoUSD = PLN / 3.5;
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
